1ff85b70145a7d9a766a2fbb998130c32a00af45
m4arthu <luisarthir259@gmail.com>
2024-02-26 18:33:10 -0300

    feat seach container

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,6 @@
 export const MainBodyBackground = "#F0F0F0"
+
+export const searchPlaceHolderColor = "#9E9E9E"
+export const searchInputColor  = "#DFDFDF"
+export const gradientPrimaryColor = "#5A84C0"
+export const gradientSecondColor = "#594ED2"
