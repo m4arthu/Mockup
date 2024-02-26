0ee53a5ccd3e97e2b5d937a5c5ff633fb56306a5
m4arthu <luisarthir259@gmail.com>
2024-02-26 20:10:36 -0300

    feat table header

--- src/pages/mainPage/style.ts
+++ src/pages/mainPage/style.ts
@@ -1,21 +1,54 @@
 import styled from  "styled-components"
-import { MainBodyBackground } from "../../config/constants"
+import { MainBodyBackground, gradientPrimaryColor, gradientSecondColor, searchInputColor, searchPlaceHolderColor } from "../../config/constants"
 
 export const Container = styled.div`
+font-family: "Roboto", sans-serif;
 width:100vw;
 height:100vh;
 display:flex;
-justify-content:center;
 align-items:center;
 background-color:${MainBodyBackground};
+flex-direction:column;
 `
 
 export const Table = styled.div`
 width:80%;
-background:black;
 `
 
+export const SearchContainer = styled.div`
+display:flex;
+justify-content:space-between;
+width:80%;
+margin-top:50px;
+margin-bottom:50px;
+align-items:end;
+position:relative;
+.searchIcon{
+    position:absolute;
+    right:10px;
+    bottom:10px;
+}
+input{
+    background:white;
+    padding-left:10px;
+    width:287px;
+    height:48px;
+    border: 1px solid ${searchInputColor};
+    border-radius:8px ;
+}
+input::placeholder{
+    color:${searchPlaceHolderColor};
+    font-weight:regular;
+    font-size:16px;
+}
+h1{
+    font-weight: 500;
+    font-size:24px;
+}
+
+`
 export const TableHeader = styled.div`
-
-
+background-image: linear-gradient(to bottom, ${gradientPrimaryColor},${gradientSecondColor});
+height:47px;
+border-radius:8px 8px 0 0;
 `
