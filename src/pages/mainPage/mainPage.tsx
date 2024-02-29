6d7e8d18286d1e23dbefc2b930cfd559ad935973
m4arthu <luisarthir259@gmail.com>
2024-02-29 15:58:44 -0300

    reafactor: tableHeader in another component

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,11 +1,12 @@
 import Header from "../../components/header/Header"
-import { Container, Table, SearchContainer, TableHeader } from "./style"
+import { Container, Table, SearchContainer} from "./style"
 import searchIcon from "../../assets/Search.svg"
 import TableLine from "../../components/tableLine/tableLine"
 import { useEffect, useRef, useState } from "react"
 import { getEmployees } from "../../services/employees"
 import { GetEmployees } from "../../helpers"
-import Elipse from "../../assets/Ellipse 1.svg"
+import TableHeaderComponent from "../../components/tableHeader/tableHeader"
+
 export default function MainPage() {
   const [employees, setEmplpyees] = useState<GetEmployees[]>([])
   const searchTerm = useRef<HTMLInputElement>(null)
@@ -63,14 +64,7 @@ export default function MainPage() {
           <img onClick={search} src={searchIcon} alt="" className="searchIcon" />
         </SearchContainer>
         <Table>
-          <TableHeader>
-            <h2 className="foto">FOTO</h2>
-            <h2 className="nome">NOME</h2>
-            <h2 className="cargo">CARGO</h2>
-            <h2 className="data">DATA DE ADMISSÂO</h2>
-            <h2 className="telefone">TELEFONE</h2>
-            <img src={Elipse} className="point" />
-          </TableHeader>
+          <TableHeaderComponent/>
           {employees.map((data) => {
             return <TableLine key={data.id} data={data} />
           })
